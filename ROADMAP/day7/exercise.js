bbe6671b0e647d5ffa3a26561d928733bac879e4
kalaikratos <kalaiarasan2796@gmail.com>
2023-12-07 14:02:49 +0100

    updated

---
+++ ROADMAP/day7/exercise.js
@@ -0,0 +1,30 @@
+/*let numbers = [3, 7, 4, 6, 9, 2];
+function cuber(value, index, array) {
+    return value ** 3;
+}
+let cube = numbers.map(cuber);
+console.log(cube);
+------------------------------------------------------
+
+let prices = [2, 3, 4, 6, 8, 9];
+let discountedPrice = prices.map (price=> {
+    if (price < 5) {
+        return price * 0.9;
+    } else if(price >5){
+        return price * 0.8;
+    }
+        else {
+        return price;
+        }
+});
+console.log(discountedPrice);
+--------------------------------------------
+let numbers = [3, 7, 4, 6, 10, 2, 11, 5];
+let divisbleFilter = (value, index, array) => {
+    if (value % 3 == 0) return true;
+}
+let divisibleNumbers = numbers.filter(divisbleFilter);
+console.log(divisibleNumbers);
+*/
+let prices = [2, 3, 4, 6, 8, 9];
+
