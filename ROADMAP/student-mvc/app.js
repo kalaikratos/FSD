59fedb4f24577ba5daad5b1eb8ffad091b7a44b8
kalaikratos <kalaiarasan2796@gmail.com>
2024-02-22 02:06:32 +0100

    updated

--- ROADMAP/student-mvc/app.js
+++ ROADMAP/student-mvc/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
 const studentController = require("./controllers/studentController");
+const mentorController = require("./controllers/mentorController");
 
 const app = express();
 require("dotenv").config();
@@ -19,6 +20,7 @@ mongoose
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(methodOverride("_method"));
 
 app.get("/", studentController.getStudents);
@@ -26,6 +28,8 @@ app.get("/students/:id/edit", studentController.getEditForm);
 app.post("/students", studentController.createStudent);
 app.put("/students/:id", studentController.updateStudent);
 app.delete("/students/:id", studentController.deleteStudent);
+
+app.post("/mentors", mentorController.createMentor);
 
 const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
