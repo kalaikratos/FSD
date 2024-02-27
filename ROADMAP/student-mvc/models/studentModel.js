b8b43228ef2342ffe62b9108d1c2bdba9194b82c
kalaikratos <kalaiarasan2796@gmail.com>
2024-02-27 02:46:43 +0100

    updated

--- ROADMAP/student-mvc/models/studentModel.js
+++ ROADMAP/student-mvc/models/studentModel.js
@@ -4,6 +4,16 @@ const studentSchema = new mongoose.Schema({
   name: String,
   age: Number,
   grade: String,
+  mentor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Mentor",
+  },
+  previousmentors: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "mentor",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Student", studentSchema);
